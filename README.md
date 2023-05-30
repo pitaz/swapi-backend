fc9661dfcbacdefa9f3cca1659457c659fdc0616
pitaz <peter.odekwo@gmail.com>
2023-05-30 09:32:11 +0100

    chore: add readme with deployment instructions

---
+++ README.md
@@ -0,0 +1,28 @@
+# Getting Started with GraphQL serverless app
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+
+
+## Deploying to AWS Lambda
+
+- Create an AWS account
+- install the AWS CLI
+- Create an IAM
+- Configure the AWS CLI with your new IAM user's credentials
+
+
+The IAM user should be granted rights to;
+
+- S3 Bucket
+- Logs
+- Lambda Functions
+- ApiGatewayV2
+
+
+Run `npm run prod:deploy` to deploy to AWS
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { ApolloServer } from '@apollo/server';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import typeDefs from './schema';
-import resolvers from './resolvers';
-import StarwarsAPI from './dataSources';
-import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
+import { ApolloServer } from "@apollo/server";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+import StarwarsAPI from "./dataSources";
+import { handlers, startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
 
 interface ContextValue {
   dataSources: {
@@ -14,21 +14,21 @@ interface ContextValue {
 let peopleSchema = makeExecutableSchema({
   typeDefs,
   resolvers,
-})
+});
 
 const server = new ApolloServer<ContextValue>({
-  schema: peopleSchema
+  schema: peopleSchema,
 });
 
 
-  export const graphqlHandler = startServerAndCreateLambdaHandler(server, handlers.createAPIGatewayProxyEventV2RequestHandler(), {
-    context: async () => {
-      const { cache } = server;
-     return {
-       dataSources: {
-        starwarsAPI: new StarwarsAPI({ cache }),
-       },
-     };
-   },
-  })
+export const graphqlHandler = startServerAndCreateLambdaHandler(server, handlers.createAPIGatewayProxyEventV2RequestHandler(), {
+  context: async () => {
+    const { cache } = server;
+   return {
+     dataSources: {
+      starwarsAPI: new StarwarsAPI({ cache }),
+     },
+   };
+ },
+});
 
