bb8af7363974e2a5d0f979c558614a94b5cdbf10
pitaz <peter.odekwo@gmail.com>
2023-05-30 10:05:35 +0100

    chore: remove unneccessary query from resolver

--- src/resolvers/people.ts
+++ src/resolvers/people.ts
@@ -1,8 +1,5 @@
 export const peopleResolvers = {
   Query: {
-    person: async (_: any, { id }:{id: string}, { dataSources }: {dataSources: any}) => {
-      return dataSources.starwarsAPI.getPerson(id);
-    },
    people: async (_: any, {limit, page, search}: IParams, { dataSources }: {dataSources: any}) => {
       return dataSources.starwarsAPI.getPeople(limit, page, search);
     },
